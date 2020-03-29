79244b42d39f57fa6646bedb46683ff8ca55d44e
Tharindra Paranagama <tharindrapara@gmail.com>
2020-03-29 13:07:19 -0700

    Final commit after updating the cachematrix.R file to inverse a given invertible matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makecacheMatrix function passes the initial matrix for which the inverse is needed and retreives the inverse of the matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  
+  #This global variable,which is used in the setInverse function is initialized to NULL,
+  #since it's value will be determined in the cacheSolve function.
+  
+  inv<-NULL
+  
+  #setMat function is used to retreive the matrix passed as a parameter to the makeCacheMatrix function
+  #and stores it in a local variable y
+  
+  setMat<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  
+  #getMat function is used to simply access the value returned by the setMat function
+  
+  getMat<-function() x
+  
+  #This function simply obtains the inverse of the matrix computed in the cacheSolve function and stores 
+  #it in the variable inv.
+  
+  setInverse<-function(inverse) inv<<-inverse
+  
+  #This function is simply used to access the value returned by the setInverse function
+  
+  getInverse<-function() inv
+  
+  #this stores the functions defined above as a list of objects
+  
+  list(setMat=setMat,getMat=getMat,setInverse=setInverse,getInverse=getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of a given matrix if not already available in cache. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #The if statement checks whether the inverse of the given matrix exist in the cache,if so it returns the inverse,
+  #if not the else block computes the inverse of the given matrix via the solve function and returns the value.
+  
+  if(!is.null(x$getInverse())){
+    
+    print("Inverse of your matrix")
+    return(x$getInverse)
+    
+  }
+  else{
+    
+    print("Inverse of your matrix")
+    print(x$setInverse(solve(x$getMat())))
+    
+  }
+  
+  
 }
+
